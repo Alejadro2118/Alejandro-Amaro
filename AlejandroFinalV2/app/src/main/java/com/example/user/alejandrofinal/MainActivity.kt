9e3e332f6e4be5b26ace41ad7ef50b87bf0d9791
Álejandro <alejandro99942@hotmail.com>
2018-11-02 22:11:31 -0600

    Versión dos
    Ya lleva base de datos con login

---
+++ AlejandroFinalV2/app/src/main/java/com/example/user/alejandrofinal/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.user.alejandrofinal
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_main.*;
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var txtNameLogin: EditText
+    private lateinit var txtPasswordLogin: EditText
+    private lateinit var progressBar: ProgressBar
+
+    private lateinit var auth:FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        txtNameLogin=findViewById(R.id.txNameLogin)
+        txtPasswordLogin=findViewById(R.id.txtContraseñaLogin)
+
+        progressBar=ProgressBar(this)
+
+
+        progressBar=findViewById(R.id.progressBar3)
+
+    auth=FirebaseAuth.getInstance()
+
+
+
+        RegistrarUser.setOnClickListener({
+            val intento1 = Intent(this@MainActivity, RegistroUsuario::class.java)
+            startActivity(intento1)
+            finish();
+        })
+    }
+    fun login(view: View){
+        loginUser()
+    }
+
+    private fun loginUser(){
+        val user:String=txtNameLogin.getText().toString()
+        val password:String=txtPasswordLogin.getText().toString()
+        if(!TextUtils.isEmpty(user) && !TextUtils.isEmpty(password)){
+            progressBar.visibility=View.VISIBLE
+
+            auth.signInWithEmailAndPassword(user, password)
+                    .addOnCompleteListener(this){
+                        task ->
+                        if(task.isSuccessful){
+                            action()
+                        }else{
+                            Toast.makeText(this, "Error al autentificar", Toast.LENGTH_LONG).show()
+                        }
+                    }
+
+        }
+    }
+private fun action(){
+    startActivity(Intent(this, HomeActivity::class.java))
+}
+
+
+}
---
+++ AlejandroFinalV2/app/src/main/java/com/example/user/alejandrofinal/RegistroUsuario.kt
@@ -0,0 +1,103 @@
+package com.example.user.alejandrofinal
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import java.security.AuthProvider
+
+class RegistroUsuario : AppCompatActivity() {
+    private lateinit var txtName:EditText
+    private lateinit var txtLastName:EditText
+    private lateinit var txtEmail:EditText
+    private lateinit var txtPassword:EditText
+    private lateinit var progressBar:ProgressBar
+    private lateinit var dbReference:DatabaseReference//firebase
+    private lateinit var database:FirebaseDatabase//para usar la bd
+    private lateinit var auth:FirebaseAuth
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro_usuario)
+        //llamammos vistas
+        txtName=findViewById(R.id.txtName)
+        txtLastName=findViewById(R.id.txtLastName)
+        txtEmail=findViewById(R.id.txtEmail)
+        txtPassword=findViewById(R.id.txtPassword)
+        progressBar=findViewById(R.id.progressBar2)
+        progressBar=ProgressBar(this)
+
+        //Instamcia para la base de dtos
+
+        database= FirebaseDatabase.getInstance()
+        auth= FirebaseAuth.getInstance()
+
+        //instancia para autenticación, con una referenica para leer o excribir en una ubicación
+        dbReference=database.reference.child("User")
+
+
+
+
+    }
+
+    fun Register(view:View){
+        createNewAccount()
+    }
+
+    private fun createNewAccount(){
+        val name:String=txtName.getText().toString()
+        val lastName:String=txtLastName.getText().toString()
+        val email:String=txtEmail.getText().toString()
+        val password:String=txtPassword.getText().toString()
+        Toast.makeText(this, "Error  Enviado"+name, Toast.LENGTH_LONG).show()
+
+
+        if(!TextUtils.isEmpty(name) && !TextUtils.isEmpty(lastName) &&!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)){
+            progressBar.visibility=View.VISIBLE
+
+            Log.d("RegistroUsuario", "ESTEEEEEEE"+email)
+            Log.d("RegistroUsuario", "ESTEEEEEEE"+password)
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this){
+                task ->
+                    if(task.isComplete){
+                        val user:FirebaseUser?=auth.currentUser
+                       // verifyEmail(user)
+                        val userBD=dbReference.child(user?.uid)
+
+                        userBD.child("Name").setValue(name)
+                        userBD.child("LastName").setValue(lastName)
+                        action()
+
+            }
+            }
+        }
+    }
+    private  fun action(){
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+   /*
+    private fun verifyEmail(user:FirebaseUser?){
+        user?.sendEmailVerification()
+                ?.addOnCompleteListener(this){
+                    task ->
+                    if (task.isComplete){
+                        Toast.makeText(this, "Email enviado", Toast.LENGTH_LONG).show()
+                    }else{
+                        Toast.makeText(this, "Error  Enviado", Toast.LENGTH_LONG).show()
+                    }
+                }
+    }*/
+}
